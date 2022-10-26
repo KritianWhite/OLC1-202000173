566080f62ebeb23173fc39fc26e1531ee7fd5e26
KritianWhite <3617836980101@ingenieria.usac.edu.gt>
2022-10-25 22:41:37 -0600

    ... Reestructurar :v

--- Proyecto 2/Proyecto/proyecto2/src/Components/pure/editor.tsx
+++ Proyecto 2/Proyecto/proyecto2/src/Components/pure/editor.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import CodeMirror from "@uiw/react-codemirror";
 import { dracula } from "@uiw/codemirror-theme-dracula";
 import { javascript } from "@codemirror/lang-javascript";
 
 export const Editor = () => {
+  
+  const [myValue, setMyValue] = useState('');
+
   return (
         <CodeMirror
           value=""
@@ -28,3 +31,4 @@ export const Editor = () => {
     // </div>
   );
 };
+
